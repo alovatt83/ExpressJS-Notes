0990dfc1d9c36400970c1d6e4a41999bcfc8c68d
Allen <allenlovatt@gmail.com>
2021-02-20 18:13:42 -0500

    Assigning Routes

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./public'));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
-app.use(express.static('./Develop/public'));
+app.use(express.static('./public'));
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
