7c5670647781ffea2a46834ee07deefaa658bc70
Allen <allenlovatt@gmail.com>
2021-02-20 15:29:04 -0500

    Set index path

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+  });
+
+app.use(express.static('./Develop/public'));
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
